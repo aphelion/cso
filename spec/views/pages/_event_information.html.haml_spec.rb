58e732d22a080938d079cadddeb9f2150b29a716
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-03-17 12:16:36 +0100

    Add a Tickets button in the Event Information section

--- spec/views/pages/_event_information.html.haml_spec.rb
+++ spec/views/pages/_event_information.html.haml_spec.rb
@@ -7,5 +7,9 @@ describe 'pages/_event_information.html.haml' do
     it 'has an anchor tag' do
       expect(rendered).to have_css('#event-information')
     end
+
+    it 'links to the Tickets page' do
+      expect(rendered).to have_link 'Tickets', tickets_status_path
+    end
   end
 end
