f3546f0c5fc6f2c7463af7a91eaa6f8050a3ac29
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-03-26 01:01:57 +0100

    Ensure Events are displayed ordered

--- app/services/events_service.rb
+++ app/services/events_service.rb
@@ -2,7 +2,7 @@ module EventsService
   extend self
 
   def upcoming_events
-    model.where('sale_start < ? AND sale_end > ?', DateTime.now, DateTime.now)
+    model.where('sale_start < ? AND sale_end > ?', DateTime.now, DateTime.now).order(event_start: :asc)
   end
 
   def model
--- spec/services/events_service_spec.rb
+++ spec/services/events_service_spec.rb
@@ -1,6 +1,7 @@
 describe EventsService do
   let(:model) { double(:Event) }
-  let(:events) { [double(:event), double(:event)] }
+  let(:events) { double(:event) }
+  let(:ordered_events) { double(:ordered_events) }
 
   describe 'object seams' do
     it { expect(subject.model).to eq(Event) }
@@ -14,10 +15,11 @@ describe EventsService do
     describe '.upcoming_events' do
       before do
         allow(model).to receive(:where).with('sale_start < ? AND sale_end > ?', DateTime.now, DateTime.now).and_return(events)
+        allow(events).to receive(:order).with(event_start: :asc).and_return(ordered_events)
       end
 
       it 'retrieves the purchasable events from the model' do
-        expect(subject.upcoming_events).to be(events)
+        expect(subject.upcoming_events).to be(ordered_events)
       end
     end
   end
