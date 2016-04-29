4a749c3ff5c9115e488f6eb797d16137623eb71c
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-04-28 21:51:45 -0500

    Don't confuse users by allowing them to add multiple addons of the same type

--- spec/views/event_purchases/_addon_selection.html.haml_spec.rb
+++ spec/views/event_purchases/_addon_selection.html.haml_spec.rb
@@ -6,27 +6,44 @@ describe 'event_purchases/_addon_selection.html.haml' do
 
   before do
     event_purchase.event = event
-
-    render partial: 'event_purchases/addon_selection', locals: {event_purchase: event_purchase}
   end
 
-  it 'renders a button for each Addon' do
-    event.addons.each do |addon|
-      assert_select 'button', "Add #{addon.name}"
-      expect(rendered).to have_text(addon.name)
+  context 'when a user already has an Addon' do
+    let(:addon) { event.addons.first }
+
+    before do
+      event_purchase.addon_purchases << ProductPurchase.new(product: addon)
+      render partial: 'event_purchases/addon_selection', locals: {event_purchase: event_purchase}
+    end
+
+    it 'does not render the button to add that Addon' do
+      expect(rendered).to_not have_button addon.name
     end
   end
 
-  it 'renders the description for each Addon' do
-    event.addons.each do |addon|
-      expect(rendered).to have_text(addon.description)
+  context 'when a user lands on the Event Purchase form' do
+    before do
+      render partial: 'event_purchases/addon_selection', locals: {event_purchase: event_purchase}
     end
-  end
 
-  describe 'the Addon modal' do
-    it 'renders a quantity dropdown menu' do
+    it 'renders a button for each Addon' do
       event.addons.each do |addon|
-        expect(rendered).to have_selector("#addon-modal-#{addon.id} [name='addon_#{addon.id}[quantity]']")
+        assert_select 'button', "Add #{addon.name}"
+        expect(rendered).to have_text(addon.name)
+      end
+    end
+
+    it 'renders the description for each Addon' do
+      event.addons.each do |addon|
+        expect(rendered).to have_text(addon.description)
+      end
+    end
+
+    describe 'the Addon modal' do
+      it 'renders a quantity dropdown menu' do
+        event.addons.each do |addon|
+          expect(rendered).to have_selector("#addon-modal-#{addon.id} [name='addon_#{addon.id}[quantity]']")
+        end
       end
     end
   end
