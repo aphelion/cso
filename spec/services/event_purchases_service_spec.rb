2c36bbd731658d50860930da8e5a800ad92982d9
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-05-18 18:53:34 -0700

    changed a unit test of a service to a integration with db test

--- spec/services/event_purchases_service_spec.rb
+++ spec/services/event_purchases_service_spec.rb
@@ -5,38 +5,25 @@ describe EventPurchasesService do
   end
 
   describe 'actions' do
-    let(:model) { double(:EventPurchase) }
-    let(:event) { double(:event) }
-    let(:user) { double(:user) }
-    let(:event_purchase) { double(:event_purchase) }
-    let(:past_event_event_purchases) { double(:past_event_event_purchases) }
+    fixtures(:all)
 
-    before do
-      allow(subject).to receive(:model).and_return(model)
-    end
+    let(:event) { events(:salsa_party) }
+    let(:user)  { users(:crystal) }
+    let(:event_purchase) { event_purchases(:crystals_salsa_party_purchase) }
+    let(:past_event_event_purchase) { event_purchases(:crystals_bachata_party_purchase) }
 
     describe '.find_by_event_and_user' do
-      before do
-        allow(model).to receive(:find_by).with(event: event, user: user).and_return(event_purchase)
-      end
-
       it 'retrieves the purchasable events from the model' do
         returned_event_purchase = subject.find_by_event_and_user(event, user)
-
-        expect(returned_event_purchase).to be(event_purchase)
+        expect(returned_event_purchase).to eq(event_purchase)
       end
     end
 
     describe '.past_event_event_purchases' do
-      before do
-        allow(model).to receive(:where).with(user: user,)
-        allow(model).to receive_message_chain(:where, :joins, :where, :order).and_return(past_event_event_purchases)
-      end
-
       it "retrieves the given User's Event Purchases for past Events" do
         returned_tickets = subject.past_event_event_purchases(user)
 
-        expect(returned_tickets).to be(past_event_event_purchases)
+        expect(returned_tickets).to include(past_event_event_purchase)
       end
     end
   end
