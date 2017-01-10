cd120dc31dcd21566521635e505c509778f8ab45
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-01-09 22:47:41 -0800

    Improve the seeds file to include some users and purchases

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,63 @@
 EventPurchase.destroy_all
+ProductPurchaseOptionChoice.destroy_all
+ProductPurchase.destroy_all
+Charge.destroy_all
+ProductOption.destroy_all
 Product.destroy_all
 Event.destroy_all
+Identity.destroy_all
+Customer.destroy_all
+User.destroy_all
+
+cso_2017 = Event.create(
+    name: '2017 Collegiate Salsa Open',
+    description: 'Returning for the second year, the Collegiate Salsa Open is the premiere salsa summit where dancers from colleges and communities all over California come together to celebrate our unique salsa scene through competitions, performances, and all-night social dancing. The CSO is organized by the college dance community for the college dance community.',
+    time_zone: 'Pacific Time (US & Canada)',
+    event_start: DateTime.new(2017, 5, 7, 16, 30),
+    event_end: DateTime.new(2017, 5, 8, 3, 30),
+    sale_start: DateTime.new(2017, 1, 1),
+    sale_end: DateTime.new(2017, 5, 7)
+)
+
+cso_2017.tickets << Product.create(
+    name: 'Full Pass',
+    price_cents: 2900,
+    description: 'A Full Pass entitles you to everything your heart has ever wanted. Get this one.'
+)
+
+spectator_ticket = Product.create(
+    name: 'Spectator Pass',
+    price_cents: 800,
+    description: "Get a Spectator Pass if you can't dance but need to see your kid do this salsa thing."
+)
+
+cso_2017.tickets << spectator_ticket
+
+cso_2017.tickets << Product.create(
+    name: 'Evening Pass',
+    price_cents: 2200,
+    description: "You're older, finished (or never went to) college, and want to come to hit on the college chicks--this one's for you."
+)
+
+cso_2017_shirt = Product.create(
+    name: 'CSO 2017 Shirt',
+    price_cents: 1200,
+    description: 'Rep the CSO! Give us money! Swag!'
+)
+
+ProductOption.create(product: cso_2017_shirt, name: 'Gender', choices: ['Male', 'Female'])
+ProductOption.create(product: cso_2017_shirt, name: 'Size', choices: ['XS', 'S', 'M', 'L', 'XL'])
+ProductOption.create(product: cso_2017_shirt, name: 'Color', choices: ['Tiedie', 'See-Through', 'Black'])
+
+cso_water_bottle = Product.create(
+    name: 'CSO Water Bottle',
+    price_cents: 900,
+    description: "Stay hydrated, fill it with Redbull vodka, we don't care. Just buy it."
+)
+
+ProductOption.create(product: cso_water_bottle, name: 'Color', choices: ['Red', 'Black'])
+
+cso_2017.addons << [cso_2017_shirt, cso_water_bottle, spectator_ticket]
 
 cso_2016 = Event.create(
     name: '2016 Collegiate Salsa Open',
@@ -12,11 +69,13 @@ cso_2016 = Event.create(
     sale_end: DateTime.new(2016, 5, 7)
 )
 
-cso_2016.tickets << Product.create(
+cso_2016_full_pass = Product.create(
     name: 'Full Pass',
     price_cents: 2900,
     description: 'A Full Pass entitles you to everything your heart has ever wanted. Get this one.'
 )
+
+cso_2016.tickets << cso_2016_full_pass
 
 spectator_ticket = Product.create(
     name: 'Spectator Pass',
@@ -80,8 +139,21 @@ cso_2015 = Event.create(
     sale_end: DateTime.new(2015, 4, 25)
 )
 
-cso_2015.tickets << Product.create(
+cso_2015_full_pass = Product.create(
     name: 'Full Pass',
     price_cents: 2800,
     description: "You're OG CSO. Thanks for the love."
 )
+
+cso_2015.tickets << cso_2015_full_pass
+
+
+me = User.create(email: 'lennykoepsell@gmail.com', first_name: 'Leonhardt', last_name: 'Koepsell')
+my_charge_2015 = Charge.create(charge_id: 'charge-1', processor: 'stripe')
+my_cso_2015_full_pass = ProductPurchase.create(product: cso_2015_full_pass, charge: my_charge_2015, quantity: 1)
+EventPurchase.create(user: me, ticket_purchase: my_cso_2015_full_pass, event: cso_2015)
+my_charge_2016 = Charge.create(charge_id: 'charge-2', processor: 'stripe')
+my_cso_2016_full_pass = ProductPurchase.create(product: cso_2016_full_pass, charge: my_charge_2016, quantity: 1)
+EventPurchase.create(user: me, ticket_purchase: my_cso_2016_full_pass, event: cso_2016)
+
+admin = User.create(email: 'leonhardt@koepsell.io', first_name: 'Leonhardt', last_name: 'Koepsell', admin: true)
